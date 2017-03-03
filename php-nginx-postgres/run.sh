e8bea28087d27f0ffcf7d63a1ba9c0d7fb34fef0
Zhou Yicheng <zhouyc@lkhealth.cn>
2017-03-03 12:03:00 +0800

    Fix env

--- php-nginx-postgres/run.sh
+++ php-nginx-postgres/run.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+export DEV_USER=${DEV_USER:-dev}
+export DEV_PASS=${DEV_PASS:-dev}
+export ROOT_PASS=${ROOT_PASS:-root}
+export POSTGRES_USER=${POSTGRES_USER:-postgres}
+export POSTGRES_PASS=${POSTGRES_PASS:-postgres}
+
+echo "root:${ROOT_PASS}" | chpasswd > /dev/null 2>&1
+echo -e "${DEV_PASS}\n${DEV_PASS}" | adduser ${DEV_USER} > /dev/null 2>&1
 /usr/sbin/sshd -o PermitRootLogin=yes -o UseDNS=no
 chown -R $DEV_USER:$DEV_USER /www /etc/nginx /etc/php7 /var/log /var/lib/nginx /var/lib/postgresql /run/postgresql /tmp /etc/s6.d
 su-exec $DEV_USER:$DEV_USER /bin/s6-svscan /etc/s6.d
--- php-nginx/run.sh
+++ php-nginx/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+export DEV_USER=${DEV_USER:-dev}
+export DEV_PASS=${DEV_USER:-dev}
+export ROOT_PASS=${ROOT_PASS:-root}
+
+echo "root:${ROOT_PASS}" | chpasswd > /dev/null 2>&1
+echo -e "${DEV_PASS}\n${DEV_PASS}" | adduser ${DEV_USER} > /dev/null 2>&1
 /usr/sbin/sshd -o PermitRootLogin=yes -o UseDNS=no
 chown -R $DEV_USER /www /etc/nginx /etc/php7 /var/log /var/lib/nginx /tmp /etc/s6.d
 exec su-exec $DEV_USER /bin/s6-svscan /etc/s6.d
