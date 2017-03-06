e50b446a2ba955fddd248cb7a27133389f2cb163
Zhou Yicheng <zyc9012@163.com>
2017-03-06 20:28:14 +0800

    Add group

--- php-nginx/run.sh
+++ php-nginx/run.sh
@@ -6,5 +6,5 @@ export ROOT_PASSWORD=${ROOT_PASSWORD:-root}
 echo "root:${ROOT_PASSWORD}" | chpasswd > /dev/null 2>&1
 echo -e "${DEV_PASSWORD}\n${DEV_PASSWORD}" | adduser ${DEV_USER} > /dev/null 2>&1
 /usr/sbin/sshd -o PermitRootLogin=yes -o UseDNS=no
-chown -R $DEV_USER /www /etc/nginx /etc/php7 /var/log /var/lib/nginx /tmp /etc/s6.d
-exec su-exec $DEV_USER /bin/s6-svscan /etc/s6.d
+chown -R $DEV_USER:$DEV_USER /www /etc/nginx /etc/php7 /var/log /var/lib/nginx /tmp /etc/s6.d
+exec su-exec $DEV_USER:$DEV_USER /bin/s6-svscan /etc/s6.d
